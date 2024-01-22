5dddb166bc6df8bb91c889cbbde80586e46310f9
Jazib Ahmad <jz346475@dal.ca>
2024-01-22 18:50:14 -0400

    update

--- src/Pages/StocksDetails.jsx
+++ src/Pages/StocksDetails.jsx
@@ -1,9 +1,49 @@
-import { useEffect } from 'react';
+import { useEffect,useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import finnHub from '../Apis/finnHub';
+import StockChart from '../componenets/StockChart'
 
 export default function StockDetails() {
     const {symbol}=useParams()
+    const [graphData, setGraphData] = useState({
+        day: [
+          { x: 1, y: 10 },
+          { x: 2, y: 20 },
+          { x: 3, y: 15 },
+          { x: 4, y: 25 },
+          { x: 5, y: 18 },
+          { x: 6, y: 30 },
+          { x: 7, y: 22 },
+          { x: 8, y: 35 },
+          { x: 9, y: 28 },
+          { x: 10, y: 40 }
+        ],
+        month: [
+          { x: 1, y: 5 },
+          { x: 2, y: 15 },
+          { x: 3, y: 8 },
+          { x: 4, y: 18 },
+          { x: 5, y: 12 },
+          { x: 6, y: 25 },
+          { x: 7, y: 20 },
+          { x: 8, y: 30 },
+          { x: 9, y: 22 },
+          { x: 10, y: 35 }
+        ],
+        year: [
+          { x: 2021, y: 100 },
+          { x: 2022, y: 120 },
+          { x: 2023, y: 90 },
+          { x: 2024, y: 110 },
+          { x: 2025, y: 130 },
+          { x: 2026, y: 105 },
+          { x: 2027, y: 125 },
+          { x: 2028, y: 145 },
+          { x: 2029, y: 120 },
+          { x: 2030, y: 150 }
+        ]
+      });
+      console.log(graphData)
 
     // useEffect(() => {
     //     const fetchData = async () => {
@@ -35,7 +75,8 @@ export default function StockDetails() {
     return (
 
         <div>
-         <a>API access denied for {symbol}</a>
+         <a>API access denied for {symbol}. Hard Coded Data is used</a>
+         {!graphData && <StockChart/>}
         </div>
       );
 }
---
+++ src/componenets/StockChart.jsx
@@ -0,0 +1,7 @@
+export default function StockChart(chartData) {
+    return(
+        <div>
+
+        </div>
+    )
+}
